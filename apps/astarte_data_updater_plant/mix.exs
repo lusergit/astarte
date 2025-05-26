59ccb397f75b28ad6959b36471cb711dc875efd3
Luca Zaninotto <luca.zaninotto@secomind.com>
2025-05-26 10:18:47 +0200

    chore(DUP): add static code analysis tools
    Adds `credo` static code analisys tool
    
    Signed-off-by: Luca Zaninotto <luca.zaninotto@secomind.com>

--- apps/astarte_data_updater_plant/mix.exs
+++ apps/astarte_data_updater_plant/mix.exs
@@ -103,6 +103,7 @@ defmodule Astarte.DataUpdaterPlant.Mixfile do
       {:telemetry, "~> 0.4"},
       {:observer_cli, "~> 1.5"},
       {:dialyxir, "~> 1.0", only: [:dev, :ci], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       # Workaround for Elixir 1.15 / ssl_verify_fun issue
       # See also: https://github.com/deadtrickster/ssl_verify_fun.erl/pull/27
       {:ssl_verify_fun, "~> 1.1.0", manager: :rebar3, override: true},
